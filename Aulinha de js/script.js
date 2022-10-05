a80eb4fd383de0ff2879d2d322c50f2082394258
PressCsion <hugo.onze09@gmail.com>
2022-10-05 16:29:50 -0300

    Port-test

---
+++ Aulinha de js/script.js
@@ -0,0 +1,19 @@
+// ArrowFuntion
+
+const changePosition = () => {
+    const element = document.getElementById("textoBase")
+    element.className = ""
+    return
+}  
+
+const changeName = () => {
+    const element = document.getElementById("textoBase")
+    element.innerHTML= "Degelo"
+    return
+}  
+
+const changeColor = () => {
+    const element = document.getElementById("textoBase")
+    element.style.color = "red"
+    return
+}  
