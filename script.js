fd99ecd0576ac7a53068906764f3bc370b489e3c
PressCsion <hugo.onze09@gmail.com>
2022-10-08 17:53:00 -0300

    Correction1

---
+++ script.js
@@ -0,0 +1,23 @@
+const changeImage1 = () => {
+    const element = document.getElementById("change-image")
+    element.src = "../Portfolio/img/image_final_1_1.png"
+    return
+}  
+
+const changeImage2 = () => {
+    const element = document.getElementById("change-image")
+    element.src = "../Portfolio/img/image_final_1_2.png"
+    return
+}  
+
+const changeImage3 = () => {
+    const element = document.getElementById("change-image")
+    element.src = "../Portfolio/img/image_final_1_3.png"
+    return
+}  
+
+const changeImage4 = () => {
+    const element = document.getElementById("change-image")
+    element.src = "../Portfolio/img/image_final_1_4.png"
+    return
+}  
